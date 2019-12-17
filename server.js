ee596b35661833ca44affbbc8eedf24ba5796c20
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2019-12-17 01:37:58 -0600

    added action and project router boilerplate

--- server.js
+++ server.js
@@ -2,26 +2,49 @@
 const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
+const morgan = require("morgan")
 
 const middleware = require("./middleware/customMiddleware");
 
 // This imports our routers
-const routes = require("./routes/index.routes");
+const actionRouter = require('./actions/actionRouter')
+const projectRouter = require('./projects/projectRouter')
+
 
 // This defines the express server
 const server = express();
 
+ 
 // This invokes our dependencies 
-// middleware(server);
-// routes(server);
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+server.use(morgan())
 
+// This invokes our routes 
+server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
+
+
+// These are our error routes
+
+// This defines our error status 500 message
+server.use((err, req, res, next) => {
+    res.status(500).json({ error, message: "Opps something went wrong, it's not you, it me." })
+})
+
+// This defines a 404 error 
+server.use((req, res) => {
+    res.status(404).json({ message: "path not found" });
+  });
+
+  
 // This verifies that API is running on our root path
 server.get("/", (req, res) => {
     res.status(200).json({ status: "The server is running, better go catch it!!!" });
   });
+
+
 
 module.exports = server;
 
