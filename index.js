5ae50b9726f3931392754246f6aa8018f2f2d34c
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-07 15:47:34 -0600

    -Root get working

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 
 const server = express()
 
-
+server.get("/", (req, res) => {
+    res.status(200).send("Hello World, from the root path")
+})
 host = "127.0.0.1"
 port = 8080
 
