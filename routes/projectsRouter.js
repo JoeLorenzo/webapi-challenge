ce2c38418cc3f05b700313f059f8d381f957b210
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-07 16:42:24 -0600

    -All projects endpoint functional.

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -1,8 +1,24 @@
 const express = require("express")
 const router = express.Router()
-
+const projects = require("../data/helpers/projectModel")
 router.use(express.json())
-router.get("/", (req, res) => {
-    res.status(200).json({Hello:"world"})
+
+// This route gets all projects
+router.get("/projects", (req, res) => {
+    projects.get()
+    .then(projects => {
+        res.status(200).json(projects)
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error:"Server error. Unable to retrieve projects"})
+    })
+})
+
+// This route get a specfic project by id
+router.get("/projects/:projectId", (req, res) => {
+    
+})
+
+
 module.exports = router 
