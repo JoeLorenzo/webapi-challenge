490da915d35e30223cc8184e1a4af5c44cfc110c
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-07 15:40:55 -0600

    -Boilerplate server running from index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const express = require("express")
+
+const server = express()
+
+
+host = "127.0.0.1"
+port = 8080
+
+server.listen(port, ()=> console.log(`server running on ${host}:${port}, better go catch it.`)) 
